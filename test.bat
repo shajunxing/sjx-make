1e35b6c76de021b09f964427bb369dbbdd69ca02
shajunxing <shajunxing@163.com>
2026-05-20 21:59:18 +0000

    Delete test.bat

--- test.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d %~dp0
-md bin
-set cc=cl /nologo /std:clatest /utf-8 /O2 /W3 /MD
-%cc% /Fobin\sleep.obj /Febin\sleep.exe src\sleep.c && %CC% /Fobin\test.obj /Febin\test.exe src\test.c && bin\test.exe %*
